7cfe9fc73f3477d891b7ed24be5b87a3508535ae
HyungJin <yhj96099@naver.com>
2022-01-11 13:08:27 +0900

    등록 API Commit

---
+++ src/main/java/com/hj/book/springboot/web/PostsApiController.java
@@ -0,0 +1,19 @@
+package com.hj.book.springboot.web;
+
+import com.hj.book.springboot.service.PostsService.PostsService;
+import com.hj.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+
+        return postsService.save(requestDto);
+    }
+}
